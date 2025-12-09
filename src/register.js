432b58165bd14e6cb4bcc4636fdce3612c329938
Vishnu-IOT <93981819+Vishnu-IOT@users.noreply.github.com>
2025-12-09 11:43:45 +0530

    Update register.js

--- src/register.js
+++ src/register.js
@@ -26,7 +26,7 @@ function Register() {
 
         // axios.post("http://localhost:3000/login", formdata);
 
-        await fetch("/admin/login", {
+        await fetch("authbackend-production-b892.up.railway.app/admin/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -76,3 +76,4 @@ function Register() {
 }
 
 export default Register
+
